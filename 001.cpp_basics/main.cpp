beb01c223428001018841726916ddff899c909d4
Manas Mishra <mmanas@vmware.com>
2021-08-23 19:56:22 +0530

    Completed basic c++ video

---
+++ 001.cpp_basics/main.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"hello world"<<endl;
+    string name = "Manas";
+    cout<<"Hello, "<<name<<endl;
+    cout<<"length="<<name.length()<<endl;
+    cout<<"substring "<<name.substr(1,23)<<endl;
+    return 0;
+}
